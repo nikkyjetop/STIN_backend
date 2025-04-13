f2a401f06a6c94592cc3661121ef864429ef1b69
Dominika Strečanská <115701063+nikkyjetop@users.noreply.github.com>
2025-04-14 00:50:54 +0200

    simple auth

--- StrecanskaBackend/StrecanskaBackend/Controllers/StockDatasController.cs
+++ StrecanskaBackend/StrecanskaBackend/Controllers/StockDatasController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StrecanskaBackend.Models;
