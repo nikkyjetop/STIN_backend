02f8e6d6badf4beff0fb95a8bbfb27f6436554af
Dominika Strečanská <115701063+nikkyjetop@users.noreply.github.com>
2025-04-14 00:25:44 +0200

    database connection

---
+++ StrecanskaBackend/StrecanskaBackend/Models/Tables/FavoriteTicker.cs
@@ -0,0 +1,10 @@
+﻿namespace StrecanskaBackend.Models.Tables
+{
+    public class FavoriteTicker
+    {
+        public int Id { get; set; }
+        public string Ticker { get; set; }
+        public string Name { get; set; }
+        public string Logo { get; set; }
+    }
+}
